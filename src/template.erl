7821b8f4048cf45a4086a381236a52dc7ad7cda9
James Judd <juddling@assassinators.net>
2016-03-03 14:43:51 +0000

    added support for more template types, fixed bug for matching 7 and 7.0. added template tests

--- src/template.erl
+++ src/template.erl
@@ -6,7 +6,7 @@
 % does the tuple conform to the supplied template
 
 
-template_match(Template, Tuple) ->
+template_match(Template, Tuple) when is_tuple(Template) and is_tuple(Tuple) ->
   template_match(Template, Tuple, 1).
 template_match(Template, Tuple, _) when tuple_size(Template) /= tuple_size(Tuple) ->
   false;
@@ -30,8 +30,21 @@ template_match_element(float, Element) ->
   is_float(Element);
 template_match_element(boolean, Element) ->
   is_boolean(Element);
+template_match_element(binary, Element) ->
+  is_binary(Element);
+template_match_element(tuple, Element) ->
+  is_tuple(Element);
+template_match_element(atom, Element) ->
+  is_atom(Element);
+template_match_element(list, Element) ->
+  is_list(Element) and not io_lib:printable_unicode_list(Element);
+template_match_element(string, Element) ->
+  % there is no is_string() function in erlang
+  % strings are just lists
+  is_list(Element) and io_lib:printable_unicode_list(Element);
 template_match_element(Value, Element) ->
-  Value == Element.
+  % strict equality operator
+  Value =:= Element.
 
 % return a tuple from a bag that matches the supplied template, if no match is found
 % return false
