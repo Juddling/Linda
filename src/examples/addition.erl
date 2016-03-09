142b0ac89f8c8afb4f069ef93762396e4510dc3f
James Judd <juddling@assassinators.net>
2016-03-09 19:05:00 +0000

    tweak addition example

--- src/examples/addition.erl
+++ src/examples/addition.erl
@@ -1,48 +1,39 @@
 -module(addition).
--export([init/0, execute/2, supervisor/2, add/1]).
+-export([init/0, start/0, add/1]).
 
--define(ADDITION_TS, addition_ts).
+start() ->
+  spawn(?MODULE, init, []).
 
-add(Pid) when is_pid(Pid) ->
-  {One} = tuple_space:in(?ADDITION_TS, {integer}),
+add(TupleSpaceName) ->
+  {One} = tuple_space:in(TupleSpaceName, {integer}),
 
   io:format("~p received a one of: ~p~n", [self(), One]),
 
-  {Two} = tuple_space:in(?ADDITION_TS, {integer}),
+  {Two} = tuple_space:in(TupleSpaceName, {integer}),
 
   io:format("~p received a two of: ~p~n", [self(), Two]),
 
-  tuple_space:out(?ADDITION_TS, {One + Two}),
+  tuple_space:out(TupleSpaceName, {One + Two}),
 
-  %% inform the supervisor process it is complete
-  Pid ! done.
-
-execute(Workers, Pid) when is_pid(Pid) ->
-  lists:map(fun(_X) -> spawn(?MODULE, add, [Pid]) end, lists:seq(1, Workers)).
+  add(TupleSpaceName).
 
 init() ->
-  N = 10,
-  _ = linda_kernel:create_ts(?ADDITION_TS, self()),
-  self() ! "init",
-  lists:map(fun(X) -> tuple_space:out(?ADDITION_TS, {X}) end, lists:seq(1, N)),
-  spawn(?MODULE, supervisor, [N, self()]).
+  N = 50,
+  Workers = 2,
 
-supervisor(TupleCount, ReplyPid) ->
-  receive
-    done ->
-      %% this means a worker has just finished
-      NewTupleCount = TupleCount - 1,
-      ReplyPid ! NewTupleCount,
+  linda_kernel:start(),
 
-      case NewTupleCount of
-        0 ->
-          %% last tuple in the ts, we can return it
-          ReplyPid ! lollolololol,
-          ReplyPid ! tuple_space:in(?ADDITION_TS, {integer});
-        _ ->
-          % TODO: don't just call the function here, spawn off a new set of workers
-          % or reply to the works to let them know they can recurse
-          add(self()),
-          supervisor(NewTupleCount, ReplyPid)
-      end
-  end.
+  % create a tuple space
+  TSName = linda_kernel:create_ts(self()),
+
+  % add numbers to TS
+  lists:map(fun(X) -> linda_kernel:out(TSName, {X}) end, lists:seq(1, N)),
+
+  % wait for the numbers to be added
+  timer:sleep(100),
+
+  linda_kernel:spawn(TSName, ?MODULE, add, [TSName]),
+
+  add(TSName).
+
+  % lists:map(fun(_X) -> linda_kernel:spawn(TSName, ?MODULE, add, [TSName]) end, lists:seq(1, Workers)).
